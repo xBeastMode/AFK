53b52a4c18cddf76d1a1dad92ffd1dd8aa77957e
Jason <jasonwynn10@gmail.com>
2016-01-17 13:01:17 -0500

    updated plugin.yml and Main.php

--- src/jasonwynn10/AFK/Main.php
+++ src/jasonwynn10/AFK/Main.php
@@ -11,41 +11,35 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\Listener;
-use pocketmine\level\Position;
 use pocketmine\level\Level;
-use pocketmine\entity\Effect;
 use pocketmine\entity\Entity;
-use pocketmine\event\player\PlayerGameModeChangeEvent;
-use pocketmine\event\player\PlayerInteractEvent;
 
 
 
 class Main extends PluginBase{
 
-    public function onEnable(){
-        $this->getLogger()->info(Color::GREEN."[TutorialPlugin] Enabled!");
+    public function onEnable()
+    {
+        $this->getLogger()->info(Color::GREEN . "[TutorialPlugin] Enabled!");
     }
 
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args)  {
-        if($sender instanceof Player) {
-            $name = $sender->getName();
-            if(strtolower($command->getName()) == "afk") {
-                if(count($args) < 1) {
-                    $Phealth = getHealth();
-                    $sender->sendMessage(Color::GREEN."[AFK plugin] You are now AFK!");
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+    {
+        if ($sender instanceof Player) {
+            if (strtolower($command->getName()) == "afk") {
+                if (count($args) < 1) {
+                    $sender->sendMessage(Color::GREEN . "[AFK plugin] You are now AFK!");
                     $sender->setHealth(20);
                     $sender->getEffect(10);
-
+                    $this->god = true;
                     return;
-                }else{
-                    $sender->sendMessage(Color::GREEN."[AFK plugin] This command has no arguments!");
+                } else {
+                    $sender->sendMessage(Color::GREEN . "[AFK plugin] This command has no arguments!");
                     return;
                 }
             }
-        }else{
-            $sender->sendMessage(Color::RED."That command can only be run in console!");
+        } else {
+            $sender->sendMessage(Color::RED . "That command can only be run in console!");
             return;
         }
     }
