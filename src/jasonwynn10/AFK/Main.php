fbfd938185808acb5c3d36496abc6a253d105847
Jason <jasonwynn10@gmail.com>
2016-01-17 13:52:36 -0500

    Updated Main.php

--- src/jasonwynn10/AFK/Main.php
+++ src/jasonwynn10/AFK/Main.php
@@ -11,9 +11,16 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\Server;
 use pocketmine\Player;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\Cancellable;
+use pocketmine\level\Position;
 use pocketmine\level\Level;
+use pocketmine\entity\Effect;
 use pocketmine\entity\Entity;
-
+use pocketmine\event\player\PlayerGameModeChangeEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 
 
 class Main extends PluginBase{
@@ -41,6 +48,12 @@ class Main extends PluginBase{
         } else {
             $sender->sendMessage(Color::RED . "That command can only be run in console!");
             return;
+        }
+    }
+
+    public function entityDamage(EntityDamageEvent $event) {
+        if($this->god == true) {
+            $event->setCancelled(true);
         }
     }
 }
