ba32f88aca50dd6829f53781f3e477b44388d416
Jason <jasonwynn10@gmail.com>
2016-01-25 17:08:58 -0500

    Added main Canceled events
    Added Death Prevention, Movement Prevention, and Item dropping
    prevention

--- src/jasonwynn10/AFK/Main.php
+++ src/jasonwynn10/AFK/Main.php
@@ -1,74 +1,76 @@
 <?php
-/**
- * Created by developer: jasonwynn10.
- */
 
-namespace AFK;
+  namespace AFK;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\utils\TextFormat as Color;
-use pocketmine\Server;
-use pocketmine\Player;
-use pocketmine\event\Listener;
-use pocketmine\level\Level;
-use pocketmine\entity\Entity;
-use pocketmine\event\entity\EntityDamageEvent;
-// use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerDropItemEvent;
-use pocketmine\event\player\PlayerRespawnEvent;
-use pocketmine\event\entity\EntityMoveEvent;
-use pocketmine\event\entity\EntityDeathEvent;
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\command\Command;
+  use pocketmine\command\CommandSender;
+  use pocketmine\utils\TextFormat as Color;
+  use pocketmine\Server;
+  use pocketmine\Player;
+  use pocketmine\event\Listener;
+  use pocketmine\level\Level;
+  use pocketmine\entity\Entity;
+  use pocketmine\event\entity\EntityDamageEvent;
+  use pocketmine\event\player\PlayerMoveEvent;
+  use pocketmine\event\player\PlayerDeathEvent;
+  use pocketmine\event\player\PlayerDropItemEvent;
 
-class Main extends PluginBase implements Listener {
-    public function onEnable() {
+
+  class Main extends PluginBase implements Listener {
+    
+      public function onEnable() {
         $this->isEnabled();
         $this->getServer()->getPluginManager()->registerEvents($this,$this);
-    }
-    public function onCommand(CommandSender $issuer,Command $cmd,$label,array $args) {
-        if(strtolower($cmd->getName()) == "afk" ) {
-            if($issuer->hasPermission("afk") or $issuer->hasPermission("afk.toggle")) {
-                $this->setEnabled();
-                if($this->isEnabled()) {
-                    $issuer->sendMessage(Color::YELLOW . "You are now AFK");
-                    $this->isDisabled();
+      }
+
+      public function onCommand(CommandSender $sender,Command $cmd,$label,array $args) {
+          if(strtolower($cmd->getName()) == "afk" ) {
+              $player_name = $this->getName();
+              if($this->isEnabled()) {
+                  $this->getServer()->broadcastMessage(Color::YELLOW . $player_name . " is no longer AFK");
+                  $this->isDisabled();
                 } else {
-                    $issuer->sendMessage(Color::YELLOW . "You are no longer AFK");
-                }
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+                  $this->setEnabled();
+                  $this->getServer()->broadcastMessage(Color::YELLOW . $player_name . " is now AFK");
+              }
+          }
+      }
     /**
      * @param EntityDamageEvent $event
-     *
-     * @priority HIGHEST
-     * @ignoreCancelled true
      */
-    public function onHurt(EntityDamageEvent $event) {
-        $entity = $event->getEntity();
-        if(($entity instanceof Player) && ($this->isEnabled() == true)) {
-            $event->setCancelled();
-        }
-        
-    public function onMove(EntityMoveEvent $event) {
-        $entity = $event->getEntity();
-        if(($entity instanceof Player) && ($this->isEnabled() == true)) {
-            $event->setCancelled();
-        }
-        
-    public function onDeath(EntityDeathEvent $event) {
-        $entity = $event->getEntity();
-        if(($entity instanceof Player) && ($this->isEnabled() == true)) {
-            $event->setCancelled();
-            
-        }
-    }
-}
+      public function onHurt(EntityDamageEvent $event) {
+          $entity = $event->getEntity();
+          if (($entity instanceof Player) && ($this->isEnabled() == true)) {
+              $event->setCancelled();
+          }
+      }
+
+    /**
+     * @param PlayerMoveEvent $event
+     */
+      public function onMove(PlayerMoveEvent $event) {
+          if ($this->isEnabled() == true) {
+              $event->setCancelled();
+          }
+      }
+
+    /**
+     * @param PlayerDeathEvent $event
+     */
+      public function onDeath(PlayerDeathEvent $event) {
+          if($this->isEnabled() == true) {
+              $event->setCancelled();
+
+          }
+      }
+
+      public function onDrop(PlayerDropItemEvent $event) {
+          if($this->isEnabled() == true) {
+              $event->setCancelled();
+
+          }
+      }
+  }
 
 ?>
