6acb9b40bd3a6f5905c7cecfddd869010be44dca
legoboy0215 <legoboy0215@users.noreply.github.com>
2016-01-29 13:25:02 +0800

    Update Main.php
    Does it work now?

--- src/AFK/Main.php
+++ src/AFK/Main.php
@@ -1,12 +1,12 @@
 <?php
 namespace AFK;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\Player;
 
-//use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\player\PlayerMoveEvent;
@@ -28,21 +28,20 @@ class Main extends PluginBase implements Listener{
         }
     }
     public function onMove(PlayerMoveEvent $event) {
-        $p = $event->getPlayer();
-        if($this->isAFK($p->getName())){
+        $p = $event->getPlayer()->getName();
+        if($this->isAFK($p){
             $event->setCancelled();
         }
     }
 
     public function onDrop(PlayerDropItemEvent $event) {
-        $p = $event->getPlayer();
-        if($this->isAFK($p->getName())){
+        $p = $event->getPlayer()->getName();
+        if($this->isAFK($p)){
             $event->setCancelled();
         }
     }
 
     private function isAFK($p){
-        $this->afk[] = $p;
         return in_array($p, $this->afk);
     }
 
@@ -55,11 +54,12 @@ class Main extends PluginBase implements Listener{
             unset($this->afk[$key]);
         }
     }
+    
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
         if(strtolower($cmd->getName()) === "afk"){
             if($sender instanceof Player) {
                 $name = $sender->getName();
-                if (!($this->isAFK($name))) {
+                if ($this->isAFK($name)) {
                     $this->disableAFK($name);
                     $this->getServer()->broadcastMessage(Color::YELLOW . $name . " is no longer AFK");
                 } else {
